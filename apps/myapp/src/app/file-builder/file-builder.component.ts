e5bb9605cd7a978dde15fcd443dd3a3c4687adca
mike davidson <mikedavidson613@gmail.com>
2019-05-19 16:39:13 -0400

    Adds resource name and type from input boxes

--- apps/myapp/src/app/file-builder/file-builder.component.ts
+++ apps/myapp/src/app/file-builder/file-builder.component.ts
@@ -5,11 +5,46 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './file-builder.component.html',
   styleUrls: ['./file-builder.component.css']
 })
+
+
 export class FileBuilderComponent implements OnInit {
 
+  fileToCreate = "main.tf";
+  resourceName=""
+  resourceType=""
+  resources = [];
   constructor() { }
 
   ngOnInit() {
   }
+
+  onResourceNameKey(value: string){
+    this.resourceName = value;
+  }
+  onResourceTypeKey(value: string){
+    this.resourceType = value;
+  }
+
+  addResource() {
+    var newTFR = new TFResource();
+    newTFR.name = this.resourceName;
+    newTFR.type = this.resourceType;
+    this.resources.push(newTFR)
+    console.log(this.resources)
+  }
+
+
+}
+
+class TFResource {
+  name = "";
+  // namePrefix  = "";
+  type = "";
+  // subType = "";
+  // policy = "";
+  // role = "";
+}
+
+class TFProvider {
 
 }
