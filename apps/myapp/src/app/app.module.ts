9bc3b0bc2c1e2e6b1a6ef844c6870ae09b463b28
mike davidson <mikedavidson613@gmail.com>
2019-05-19 17:51:14 -0400

    Removed test component

--- apps/myapp/src/app/app.module.ts
+++ apps/myapp/src/app/app.module.ts
@@ -3,21 +3,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { MycomponentComponent } from './mycomponent/mycomponent.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { FileBuilderComponent } from './file-builder/file-builder.component';
 
 const appRoutes: Routes = [
-  { path: 'mycomponent', component: MycomponentComponent, data: {} },
-  { path: 'homepage', component: HomepageComponent, data: {} },
   { path: 'filebuilder', component: FileBuilderComponent},
+
+  { path: 'homepage', component: HomepageComponent, data: {} },
   { path: '', component: HomepageComponent, data: {} },
+  
   { path: '**', component: PagenotfoundComponent }
 ]
 
 @NgModule({
-  declarations: [AppComponent, MycomponentComponent, HomepageComponent, PagenotfoundComponent, FileBuilderComponent],
+  declarations: [AppComponent, HomepageComponent, PagenotfoundComponent, FileBuilderComponent],
   imports: [
     RouterModule.forRoot(
       appRoutes,
