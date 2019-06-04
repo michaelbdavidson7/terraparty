49776c68797d26b8d7eb65d58cfc3759e62060b4
mike davidson <mikedavidson613@gmail.com>
2019-06-03 22:03:59 -0400

    Get property object's main attributes

--- apps/myapp/src/app/import-fields-scripts/soupToGetTFDocsLinks.py
+++ apps/myapp/src/app/import-fields-scripts/soupToGetTFDocsLinks.py
@@ -1,16 +1,18 @@
-print("hellow world")
-import os
-
-import os
+import urllib.error
+import urllib.request
 from bs4 import BeautifulSoup
+import os
+import json
+print("hellow world")
+
 # import urllib2
-import urllib.request, urllib.error
 
 docsLinksFileName = "tfDocsLinks.txt"
+docsLinksFileNameParsed = "tfDocsLinksParsed.txt"
 objectToExport = []
 
 
-# todo get all the sidebar resources links to go through 
+# todo get all the sidebar resources links to go through
 def getAllLinks():
     url = "https://www.terraform.io/docs/providers/aws/index.html"
 
@@ -33,24 +35,61 @@ def getAllLinks():
 
     # print(soup.prettify())
 
-def goThroughLinks():
+
+def improveLinks():
     validLinks = []
     with open(docsLinksFileName) as f:
         data = f.read()
         lines = data.split('\n')
+
+        with open(docsLinksFileNameParsed, "a") as parsedListFile:
+            for line in lines:
+                if line.startswith('/docs/providers/aws/d/') or line.startswith('/docs/providers/aws/r/'):
+                    validLinks.append(line)
+                    print(line, file=parsedListFile)
+
+    print(validLinks)
+
+
+def getResourceWebpages():
+    with open(docsLinksFileNameParsed) as f:
+        data = f.read()
+        lines = data.split('\n')
         for line in lines:
-            if line.startswith('/docs/'):
-                validLinks.append(line)
+            url = "https://www.terraform.io" + line
+            print('url: ', url)
+            content = urllib.request.urlopen(url).read()
+
+            soup = BeautifulSoup(content, features="html.parser")
+            ul = soup.find(id='inner').ul.find_all('li')
+            for li in ul:
+                # print(li)
+                strLi = str(li)
+                propertyObject = {'name':"", 'required': False, 'default': ''}
+                propertyObject['name'] = li.code.contents[0]
+                if '(Required)' in strLi:
+                    print(propertyObject['name'] + ' is required' )
+                    propertyObject['required'] = True
+                if '(Default: ' in strLi:
+                    propertyObject['default'] = strLi.split('(Default')[1]
+                propertyObject['description'] = strLi.split(')')[1].split('</li>')[0]
+                propertyObjectJson = json.dumps(propertyObject)
+                print(propertyObjectJson)
+            # print(ul)
+            # for u in ul:
+            #     print(u)
+            return soup
 
 
 def parseContent(content):
     newProperty = {
-      "key": '',
-      "value": '',
-      "dataType": "",
-      "required":False,
-      "docsUrl":""
-      }
+        "key": '',
+        "value": '',
+        "dataType": "",
+        "required": False,
+        "docsUrl": ""
+    }
+
 
 class TFResource:
     type = ""
@@ -58,6 +97,6 @@ class TFResource:
     Properties = []
 
 # class TFProperty:
-    
-        
-# goThroughLinks()
+
+
+getResourceWebpages()
