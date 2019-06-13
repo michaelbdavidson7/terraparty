05623c737ef788294b3859a1d64817d1e1921a29
mike davidson <mikedavidson613@gmail.com>
2019-06-13 00:36:59 -0400

    somehow it used the wrong import script?!?

--- apps/myapp/src/app/file-builder/file-builder.component.ts
+++ apps/myapp/src/app/file-builder/file-builder.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import json from  '../import-fields-scripts/resourcesOutputFile.json';
+import json from  '../import-fields-scripts/resourcesOutputFile3.json';
 console.log(json.length)
 
 @Component({   
