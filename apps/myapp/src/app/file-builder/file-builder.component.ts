d715c479a0c2f17664d8837b4d32c6d4678f5abb
mike davidson <mikedavidson613@gmail.com>
2019-06-18 23:45:18 -0400

    Removed unused sample

--- apps/myapp/src/app/file-builder/file-builder.component.ts
+++ apps/myapp/src/app/file-builder/file-builder.component.ts
@@ -3,29 +3,6 @@ import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import json from  '../import-fields-scripts/resourcesOutputFile3.json';
 console.log(json.length)
 
-@Component({
-  selector: 'ngbd-modal-content',
-  template: `
-    <div class="modal-header">
-      <h4 class="modal-title">Hi there!</h4>
-      <button type="button" class="close" aria-label="Close" (click)="activeModal.dismiss('Cross click')">
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>
-    <div class="modal-body">
-      <p>Hello, {{name}}!</p>
-    </div>
-    <div class="modal-footer">
-      <button type="button" class="btn btn-outline-dark" (click)="activeModal.close('Close click')">Close</button>
-    </div>
-  `
-})
-export class NgbdModalContent {
-  @Input() name;
-
-  constructor(public activeModal: NgbActiveModal) {}
-}
-
 @Component({   
   selector: 'myworkspace-file-builder',
   templateUrl: './file-builder.component.html',
